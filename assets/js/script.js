141969fea3646eb92427d4faa76b7f3990b310ed
Paul Emmerton <mrpremmerton@hotmail.com>
2024-07-03 16:54:14 +0000

    added first function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
+const nextButton = document.getElementById('next-btn');
+const questionBoxElement = document.getElementById('question-box');
+const questionElement = document.getElementById('question');
+const answersElement = document.getElementById('answers');
+const scoreAreaElement = document.getElementById('score-area');
+const scorePointsElement = document.getElementById('score-points');
 
+let randomQuestions, currentQuestionIndex;
+
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+});
+
+function startGame() {
+    startButton.classlList.add('hide');
+    randomQuestions = questions.sort(() => Math.random() - .5 );
+    currentQuestionIndex = 0;
+    questionElement.classList.remove('hide');
+    scoreArea.classList.remove('hide');
+    setNextQuestion();
+    scorePointsElement.textContent = 0;
+}
